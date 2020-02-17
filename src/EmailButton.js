3c17ed806c5ef1ab023984948ceaf84556d49c0f
josephtaylor1 <joetaylor2252>
2020-02-17 13:57:38 -0500

    refactored GamesList into a simple select option and added it to the EmailButton component

--- src/EmailButton.js
+++ src/EmailButton.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
+import GamesList from './GamesList.js';
 import './EmailButton.css';
 
 class EmailButton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            chosenGame: [
+                {
+                    id: 0,
+                    title: 'Smite'
+                },
+                {
+                    id: 1,
+                    title: 'Rainbow 6'
+                }
+            ]
+        }
+    }
 
     render() {
       return (
@@ -11,6 +27,7 @@ class EmailButton extends Component {
             <input id="text-input" type="email" value={this.props.value} onChange={this.props.onChange} />
           </label>
           <input id="button" type="submit" value="Submit" />
+          <GamesList chosenGame={this.state.chosenGame}/>
         </form>
       );
     }
--- src/EmailTableConstructor.js
+++ src/EmailTableConstructor.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import EmailButton from './EmailButton.js';
 import EmailList from './EmailList.js';
-import GamesList from './GamesList.js';
 
 class EmailTableConstructor extends Component {
     constructor(props) {
         super(props);
         this.state = {
             email: '',
-            emails: []
+            emails: [],
         }
     }
 
@@ -27,18 +26,11 @@ handleSubmit = (e) => {
     e.preventDefault();
 }
 
-toggleList(){
-    this.setState(prevState => ({
-        listOpen: !prevState.listOpen
-    }))
-}
-
   render() {
     return (
       <div>
         <EmailList emails={this.state.emails}/>
         <EmailButton value={this.state.email} onSubmit={this.handleSubmit} onChange={this.handleChange}/>
-        <GamesList title='Select a game' list={this.state.chosenGame} onClick={this.toggleList}/>
       </div>
     );
   }
